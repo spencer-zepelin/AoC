f5e5f651fe11b208ce9e5957cd25f099de04d4c1
Spencer Zepelin <spencer.zepelin@gmail.com>
2020-12-31 13:22:18 -0600

    day 7 solutions

---
+++ 2020/src/q07.py
@@ -0,0 +1,39 @@
+with open("2020/res/in07.txt") as file:
+    rules = file.read().split("\n")
+
+# Build Graphs
+contained = {}
+inverse = {}
+for rule in rules:
+    outer, inners = rule.split(" bags contain ")
+    inners = inners[:-1].split(", ")
+    iCurr = inverse.get(outer, [])
+    for inner in inners:
+        if inner[0].isdigit():
+            num, inner = int(inner.split(" ")[0]), inner[2:].split(" bag")[0]
+            current = contained.get(inner, set())
+            current.add(outer)
+            contained[inner] = current
+            iCurr.append((inner, num))
+    inverse[outer] = iCurr
+
+# BFS
+visited = set()
+queue = ["shiny gold"]
+while len(queue) > 0:
+    current = queue.pop(0)  # deque would be more performant than array
+    for neighbor in contained.get(current, []):
+        if neighbor not in visited:
+            visited.add(neighbor)
+            queue.append(neighbor)
+
+# Could memoize recursion for performance improvement
+def nest(color, graph):
+    total = 0
+    for bag in graph.get(color, []):
+        total += bag[1] * (1 + nest(bag[0], graph))
+    return total
+
+
+print("Part 1: ", len(visited))
+print("Part 2: ", nest("shiny gold", inverse))
